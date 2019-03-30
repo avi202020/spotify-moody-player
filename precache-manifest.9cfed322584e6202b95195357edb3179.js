07576043b018655168bfb0d0bfd5bfdf185515c3
shaggun <shaggynetworks@gmail.com>
2019-03-30 13:28:47 -0600

    Updates

---
+++ precache-manifest.9cfed322584e6202b95195357edb3179.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1602b48119ad27e2bee8107715c981c9",
+    "url": "/spotify-moody-player/static/media/cover.1602b481.jpg"
+  },
+  {
+    "revision": "84d7c9419e607a6fe4a7",
+    "url": "/spotify-moody-player/static/js/runtime~main.84d7c941.js"
+  },
+  {
+    "revision": "15d0d6cb33d90c7f7f28",
+    "url": "/spotify-moody-player/static/js/main.15d0d6cb.chunk.js"
+  },
+  {
+    "revision": "975a96cc939c9e9d57a4",
+    "url": "/spotify-moody-player/static/js/2.975a96cc.chunk.js"
+  },
+  {
+    "revision": "15d0d6cb33d90c7f7f28",
+    "url": "/spotify-moody-player/static/css/main.84377f03.chunk.css"
+  },
+  {
+    "revision": "c2368a1712ed6e05f27ab654c64610b0",
+    "url": "/spotify-moody-player/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/shaggun/spotify-moody-player/precache-manifest.9ff4aaae366a2aa9c0eea4fe5aefe389.js"
+  "/spotify-moody-player/precache-manifest.9cfed322584e6202b95195357edb3179.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/shaggun/spotify-moody-player/index.html", {
+workbox.routing.registerNavigationRoute("/spotify-moody-player/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
